359457c88400a6d07a632903d7aeecf72f6a58f9
Abhijith K <iamabhijith.k@gmail.com>
2019-06-03 22:14:20 +0530

    add aws postgresdb conf in settings

--- PassBYOP/settings.py
+++ PassBYOP/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'PassBYOP.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'passbyopdb',
+        'USER': 'passbyopadmin',
+        'PASSWORD': 'passbyoppassword',
+        'HOST': '35.154.159.76',
+        'PORT': '5432',
+        }
     }
-}
+
 
 
 # Password validation
